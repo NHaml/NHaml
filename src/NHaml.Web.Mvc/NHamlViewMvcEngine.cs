e886f0fa30265af3ba7cf84e22198602ae0fe3fd
lanwin.de <lanwin.de@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2008-10-24 14:59:45 +0000

    Fix #12 - Added System.Web.Mvc.Html to the viewengine

--- src/NHaml.Web.Mvc/NHamlViewMvcEngine.cs
+++ src/NHaml.Web.Mvc/NHamlViewMvcEngine.cs
@@ -7,127 +7,119 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.UI;
-
 using Microsoft.Web.Mvc;
-
 using NHaml.Engine;
 
 namespace NHaml.Web.Mvc
 {
-  [AspNetHostingPermission(SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal)]
-  [AspNetHostingPermission(SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal)]
-  public class NHamlViewMvcEngine : ViewEngine<NHamlCompiledMvcView, ControllerContext, INHamlMvcView, ViewDataDictionary>,
+  [AspNetHostingPermission( SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal )]
+  [AspNetHostingPermission( SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal )]
+  public class NHamlViewMvcEngine :
+    ViewEngine<NHamlCompiledMvcView, ControllerContext, INHamlMvcView, ViewDataDictionary>,
     IViewEngine
   {
     public NHamlViewMvcEngine()
     {
-      TemplateCompiler.AddUsing("System.Web");
-      TemplateCompiler.AddUsing("System.Web.Mvc");
-      TemplateCompiler.AddUsing("System.Web.Routing");
+      TemplateCompiler.AddUsing( "System.Web" );
+      TemplateCompiler.AddUsing( "System.Web.Mvc" );
+      TemplateCompiler.AddUsing( "System.Web.Mvc.Html" );
+      TemplateCompiler.AddUsing( "System.Web.Routing" );
 
-      TemplateCompiler.AddUsing("NHaml.Web.Mvc");
+      TemplateCompiler.AddUsing( "NHaml.Web.Mvc" );
 
       TemplateCompiler.ViewBaseType = typeof(NHamlMvcView<>);
 
-      TemplateCompiler.AddReference(typeof(UserControl).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(RouteValueDictionary).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(DataContext).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(LinkExtensions).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(Action).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(Expression).Assembly.Location);
-      TemplateCompiler.AddReference(typeof(IView).Assembly.Location);
+      TemplateCompiler.AddReference( typeof(UserControl).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(RouteValueDictionary).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(DataContext).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(LinkExtensions).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(Action).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(Expression).Assembly.Location );
+      TemplateCompiler.AddReference( typeof(IView).Assembly.Location );
 
-      foreach (var name in System.Reflection.Assembly.GetCallingAssembly().GetReferencedAssemblies())
-      {
-        TemplateCompiler.AddReference(System.Reflection.Assembly.Load(name).Location);
-      }
+      foreach( var name in System.Reflection.Assembly.GetCallingAssembly().GetReferencedAssemblies() )
+        TemplateCompiler.AddReference( System.Reflection.Assembly.Load( name ).Location );
 
       TemplateCompiler.LoadFromConfiguration();
     }
 
     #region IViewEngine Members
 
-    public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName)
+    public ViewEngineResult FindPartialView( ControllerContext controllerContext, string partialViewName )
     {
-      return new ViewEngineResult(FindAndCreatePartialView(partialViewName, controllerContext), this);
+      return new ViewEngineResult( FindAndCreatePartialView( partialViewName, controllerContext ), this );
     }
 
-    public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName)
+    public ViewEngineResult FindView( ControllerContext controllerContext, string viewName, string masterName )
     {
-      return new ViewEngineResult(FindAndCreateView(viewName, masterName, controllerContext), this);
+      return new ViewEngineResult( FindAndCreateView( viewName, masterName, controllerContext ), this );
     }
 
-    public void ReleaseView(ControllerContext controllerContext, IView view)
+    public void ReleaseView( ControllerContext controllerContext, IView view )
     {
     }
 
     #endregion
 
-    protected override NHamlCompiledMvcView CreateView(string viewName, string layoutName, ControllerContext context)
+    protected override NHamlCompiledMvcView CreateView( string viewName, string layoutName, ControllerContext context )
     {
       var templatePath = context.HttpContext.Request
-        .MapPath("~/Views/" + GetViewKey(viewName, context) + ".haml");
+        .MapPath( "~/Views/" + GetViewKey( viewName, context ) + ".haml" );
 
-      var masterPath = SelectLayout(layoutName, context);
+      var masterPath = SelectLayout( layoutName, context );
 
       return new NHamlCompiledMvcView(
         TemplateCompiler,
         templatePath,
         masterPath,
-        context.Controller.ViewData);
+        context.Controller.ViewData );
     }
 
-    protected override NHamlCompiledMvcView CreatePartialView(string viewName, ControllerContext context)
+    protected override NHamlCompiledMvcView CreatePartialView( string viewName, ControllerContext context )
     {
       var templatePath = context.HttpContext.Request
-        .MapPath("~/Views/" + GetViewKey(viewName, context) + ".haml");
+        .MapPath( "~/Views/" + GetViewKey( viewName, context ) + ".haml" );
 
       return new NHamlCompiledMvcView(
         TemplateCompiler,
         templatePath,
         null,
-        context.Controller.ViewData);
+        context.Controller.ViewData );
     }
 
-    protected override string GetViewKey(string viewName, ControllerContext context)
+    protected override string GetViewKey( string viewName, ControllerContext context )
     {
-      return GetControllerName(context) + "/" + viewName;
+      return GetControllerName( context ) + "/" + viewName;
     }
 
-    protected override ViewDataDictionary GetViewData(ControllerContext context)
+    protected override ViewDataDictionary GetViewData( ControllerContext context )
     {
       return context.Controller.ViewData;
     }
 
-    protected static string SelectLayout(string layoutName, RequestContext requestContext)
+    protected static string SelectLayout( string layoutName, RequestContext requestContext )
     {
-      var layoutsFolder = requestContext.HttpContext.Request.MapPath("~/Views/Shared");
+      var layoutsFolder = requestContext.HttpContext.Request.MapPath( "~/Views/Shared" );
 
       var layoutPath = layoutsFolder + "\\" + layoutName + ".haml";
 
-      if (File.Exists(layoutPath))
-      {
+      if( File.Exists( layoutPath ) )
         return layoutPath;
-      }
 
-      layoutPath = layoutsFolder + "\\" + GetControllerName(requestContext) + ".haml";
+      layoutPath = layoutsFolder + "\\" + GetControllerName( requestContext ) + ".haml";
 
-      if (File.Exists(layoutPath))
-      {
+      if( File.Exists( layoutPath ) )
         return layoutPath;
-      }
 
       layoutPath = layoutsFolder + "\\application.haml";
 
-      if (File.Exists(layoutPath))
-      {
+      if( File.Exists( layoutPath ) )
         return layoutPath;
-      }
 
       return null;
     }
 
-    private static string GetControllerName(RequestContext requestContext)
+    private static string GetControllerName( RequestContext requestContext )
     {
       return (string)requestContext.RouteData.Values["controller"];
     }
