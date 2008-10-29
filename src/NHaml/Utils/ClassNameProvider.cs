112f336dd1ecd9f23c86e87d7f68b572d407f7cc
lanwin.de <lanwin.de@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2008-10-29 18:26:57 +0000

    Fix a wrong / in ClassNameProvider which couse the tests to fail.

--- src/NHaml/Utils/ClassNameProvider.cs
+++ src/NHaml/Utils/ClassNameProvider.cs
@@ -5,12 +5,8 @@ namespace NHaml.Utils
 {
   public static class ClassNameProvider
   {
-    /*
-     *     private static readonly Regex _pathCleaner
-      = new Regex( @"[-\\/\.:\s]", RegexOptions.Compiled | RegexOptions.Singleline );
-*/
     private static readonly Regex _pathCleaner
-      = new Regex( @"[^0-9a-z/_]", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline );
+      = new Regex( @"[^0-9a-z_]", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline );
 
     public static string MakeClassName( string templatePath )
     {
