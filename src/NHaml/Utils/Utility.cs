e04c789754c7dd4478910020d457a08e9e1cb8f4
lanwin.de <lanwin.de@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-03-14 10:51:07 +0000

    Removed unused methods, since append of attributes is noce compiler specific.

--- src/NHaml/Utils/Utility.cs
+++ src/NHaml/Utils/Utility.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Globalization;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace NHaml.Utils
@@ -39,51 +37,6 @@ namespace NHaml.Utils
             }
 
             return typeName;
-        }
-
-        public static string RenderAttributes( object attributeSource )
-        {
-            if( attributeSource != null )
-            {
-                var properties = TypeDescriptor.GetProperties( attributeSource );
-
-                if( properties.Count > 0 )
-                {
-                    var attributes = new StringBuilder();
-
-                    AppendAttribute( attributeSource, properties[0], attributes, null );
-
-                    for( var i = 1; i < properties.Count; i++ )
-                    {
-                        AppendAttribute( attributeSource, properties[i], attributes, " " );
-                    }
-
-                    return attributes.ToString();
-                }
-            }
-
-            return null;
-        }
-
-        private static void AppendAttribute( object obj, PropertyDescriptor propertyDescriptor,
-          StringBuilder attributes, string separator )
-        {
-            var value = propertyDescriptor.GetValue( obj );
-            var name = propertyDescriptor.Name.Replace( '_', '-' );
-
-            AppendAttribute( value, attributes, separator, name );
-        }
-
-        private static void AppendAttribute( object value, StringBuilder attributes,
-          string separator, object name )
-        {
-            var invariantValue = Convert.ToString( value, CultureInfo.InvariantCulture );
-            var invariantName = Convert.ToString( name, CultureInfo.InvariantCulture );
-
-            if( !string.IsNullOrEmpty( invariantValue ) )
-            {
-                attributes.Append( separator + invariantName + "=\"" + invariantValue + "\"" );
-            }
         }
 
         public static string FormatCurrentCulture( string format, params object[] values )
