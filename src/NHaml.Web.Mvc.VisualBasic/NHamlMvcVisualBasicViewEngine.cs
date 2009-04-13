9bb82e5a94a4bf410c19c777f41602a4e4b7703a
simon.cropp <simon.cropp@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-04-13 10:38:56 +0000

    add VB MVC engine

---
+++ src/NHaml.Web.Mvc.VisualBasic/NHamlMvcVisualBasicViewEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Permissions;
+using System.Web;
+using NHaml.Compilers.VisualBasic;
+
+namespace NHaml.Web.Mvc.VisualBasic
+{
+    [AspNetHostingPermission( SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal )]
+    [AspNetHostingPermission( SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal )]
+    public class NHamlMvcVisualBasicViewEngine : NHamlMvcViewEngine
+    {
+        public NHamlMvcVisualBasicViewEngine()
+        {
+            TemplateEngine.TemplateCompiler = new VisualBasicTemplateCompiler();
+
+            TemplateEngine.AddReference(typeof(NHamlMvcVisualBasicViewEngine).Assembly);
+
+        }
+
+    }
+}
