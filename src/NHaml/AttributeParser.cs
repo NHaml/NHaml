8ef339308c862b0df7d9905e0e1949ef1da2a589
lanwin.de <lanwin.de@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-04-29 08:34:36 +0000

    Re add correct version of AttributeParser which get los after merge.

--- src/NHaml/AttributeParser.cs
+++ src/NHaml/AttributeParser.cs
@@ -1,183 +1,34 @@
 ﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.IO;
+using System.Xml;
 
 namespace NHaml
 {
     public class AttributeParser
     {
-        public static Regex StartCodeRegex;
-        public static Regex StartTextWithDoubleQuotesRegex;
-        public static Regex StartTextWithSingleQuotesRegex;
-        public static Regex StartTextWithOutQuotesRegex;
-        public static Regex KeyValueSplitRegex;
-
         private readonly string attributes;
+        XmlReader reader;
 
-        private State? state;
-        private string currentValue;
-        private string currentKey;
-        private bool isCurrentCode;
-        private bool isSingleQuotes;
-        private int index;
-
-        private enum State
-        {
-            Code,
-            Text,
-            Key
-        }
-
-        static AttributeParser()
-        {
-            StartCodeRegex = new Regex(@"[\w:]*(?<ToTrim>\s*=\s*\#{)$");
-            KeyValueSplitRegex = new Regex(@"[\w:]*(?<ToTrim>\s*,\s*)$");
-            StartTextWithDoubleQuotesRegex = new Regex("[\\w:]*(?<ToTrim>\\s*=\\s*\")$");
-            StartTextWithSingleQuotesRegex = new Regex("[\\w:]*(?<ToTrim>\\s*=\\s*\')$");
-            StartTextWithOutQuotesRegex = new Regex(@"[\w:]*(?<ToTrim>\s*=.*)$");
-        }
 
         // So at the moment, I'm leaning towards {foo="bar"} or {foo=bar} for
         // static attributes and {foo=#{bar}} for dynamic ones. Thoughts?
         public AttributeParser(string attributes)
         {
             this.attributes = attributes.TrimStart();
-            Values = new List<KeyValuePair>();
         }
+
+        public Dictionary<string, string> Values { get; set; }
 
         public void Parse()
         {
-            for (index = attributes.Length - 1; index > -1; )
+            Values = new Dictionary<string, string>();
+            reader = XmlReader.Create(new StringReader(string.Format("<node {0}/>", attributes)));
+            reader.MoveToContent();
+            while (reader.MoveToNextAttribute())
             {
-                var character = attributes[index];
-                if (state == null)
-                {
-                    currentValue = null;
-                    currentKey = null;
-                    if (character == '}')
-                    {
-                        state = State.Code;
-                        isCurrentCode = true;
-                    }
-                    else if (character != ' ' && character != '\t')
-                    {
-                        if (character == '"')
-                        {
-                            isSingleQuotes = false;
-                        }
-                        else if (character == '\'')
-                        {
-                            isSingleQuotes = true;
-                        }
-                        state = State.Text;
-                        isCurrentCode = false;
-                    }
-                    index--;
-                }
-                else
-                {
-                    switch (state.Value)
-                    {
-                        case (State.Code):
-                            {
-                                ProcessCodeChar(character);
-                                break;
-                            }
-                        case (State.Key):
-                            {
-                                ProcessKeyChar(character);
-                                break;
-                            }
-                        case (State.Text):
-                            {
-                                ProcessTextChar(character);
-                                break;
-                            }
-                    }
-                }
+                Values.Add(reader.Name, reader.Value);
             }
         }
 
-        private void ProcessTextChar(char character)
-        {
-
-            var substring = attributes.Substring(0, index + 1);
-            Regex regex;
-            if (isSingleQuotes)
-            {
-                regex = StartTextWithSingleQuotesRegex;
-            }
-            else
-            {
-                regex = StartTextWithDoubleQuotesRegex;
-            }
-            var group = regex.Match(substring).Groups["ToTrim"];
-            if (group.Success)
-            {
-                index = index - group.Value.Length;
-                state = State.Key;
-                return;
-            }
-
-            currentValue = character + currentValue;
-            index--;
-        }
-
-        private void ProcessKeyChar(char character)
-        {
-
-            var substring = attributes.Substring(0, index + 1);
-            var group = KeyValueSplitRegex.Match(substring).Groups["ToTrim"];
-            if (group.Success)
-            {
-                AddCurrentToValues();
-                state = null;
-                index = index - group.Value.Length;
-                return;
-            }
-
-            currentKey = character + currentKey;
-            if (index == 0)
-            {
-                AddCurrentToValues();
-                state = null;
-                return;
-            }
-            index--;
-        }
-
-        private void AddCurrentToValues()
-        {
-            Values.Insert(0, new KeyValuePair
-            {
-                IsCode = isCurrentCode,
-                Key = currentKey,
-                Value = currentValue
-            });
-        }
-
-        private void ProcessCodeChar(char character)
-        {
-            var substring = attributes.Substring(0, index + 1);
-            var group = StartCodeRegex.Match(substring).Groups["ToTrim"];
-           
-            if (group.Success)
-            {
-                index = index - group.Value.Length;
-                state = State.Key;
-                return;
-            }
-
-            currentValue = character + currentValue;
-            index--;
-        }
-
-        public List<KeyValuePair> Values { get; set; }
-
-        public class KeyValuePair
-        {
-            public string Key;
-            public string Value;
-            public bool IsCode;
-        }
     }
 }
