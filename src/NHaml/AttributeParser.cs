b0403b400b318e1990254b54fefb1336dc1dece6
lanwin.de <lanwin.de@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-04-30 12:01:44 +0000

    AttributeParser: Give the user a hint where the parser error is. Add tests for Schema parsing and fix bug for it. Allow references to contain a . to allow a=Model.Foo .

--- src/NHaml/AttributeParser.cs
+++ src/NHaml/AttributeParser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 using NHaml.Exceptions;
 
@@ -18,7 +19,7 @@ namespace NHaml
 
             var pattern = "";
 
-            pattern += @"(?:(?<schema>\w):)?"; // schema
+            pattern += @"(?:(?<schema>\w+)\:)?"; // schema
 
             pattern += @"(?<name>\w+)"; // attribute name
 
@@ -26,8 +27,7 @@ namespace NHaml
 
             pattern += @"\s*=\s*"; // equal
 
-            //Todo: add support for a.b.c and _ when 
-            pattern += @"(?:(?<rvalue>\w+)"; // reference as value
+            pattern += @"(?:(?<rvalue>(\.|\w)+)"; // reference as value
             pattern += "|";
             pattern += @"(?:""(?<svalue>((?:\\"")|[^""])*)"")"; // double quotes
             pattern += "|";
@@ -57,14 +57,14 @@ namespace NHaml
         {
             _attributes.Clear();
 
-            // ugly, but it works
-            //Todo: better is to check if there are spaces between the maches and throw only them
-            if( _parser.Replace(attributesString, string.Empty).Length > 0 )
-                throw new SyntaxException(string.Format("Not valid attribute found: {0}", attributesString));
+            var matches = _parser.Matches(attributesString);
 
-            foreach (Match match in _parser.Matches(attributesString))
+            CheckMatches(matches);
+
+            foreach (Match match in matches)
             {
                 if (!match.Success)
+                    //Todo: put message in resource
                     throw new SyntaxException(string.Format("Attribute '{0}' is not valid.", match.Value));
 
                 var groupSchema = match.Groups["schema"];
@@ -73,22 +73,22 @@ namespace NHaml
                 var groupReferenceValue = match.Groups["rvalue"];
                 var groupDynamicValue = match.Groups["dvalue"];
 
-                var schmea = groupSchema != null ? groupSchema.Value : null;
+                var schmea = groupSchema.Success ? groupSchema.Value : null;
                 var name = groupName.Value;
                 string value;
                 NHamlAttributeType type;
 
-                if( groupStringValue.Success )
+                if (groupStringValue.Success)
                 {
                     type = NHamlAttributeType.String;
                     value = groupStringValue.Value;
                 }
-                else if( groupReferenceValue.Success )
+                else if (groupReferenceValue.Success)
                 {
                     type = NHamlAttributeType.Reference;
                     value = groupReferenceValue.Value;
                 }
-                else if( groupDynamicValue.Success )
+                else if (groupDynamicValue.Success)
                 {
                     type = NHamlAttributeType.Expression;
                     value = groupDynamicValue.Value;
@@ -101,6 +101,43 @@ namespace NHaml
 
                 _attributes.Add(new NHamlAttribute(schmea, name, value, type));
             }
+        }
+
+        private void CheckMatches(MatchCollection matches)
+        {
+            var currentIndex = 0;
+
+            foreach (Match match in matches)
+            {
+                CheckMatch(match.Index, currentIndex);
+
+                currentIndex = match.Index + match.Length;
+            }
+
+            if (currentIndex != attributesString.Length)
+            {
+                CheckMatch(attributesString.Length, currentIndex);
+            }
+        }
+
+        private void CheckMatch(int matchIndex, int currentIndex)
+        {
+            if (matchIndex == currentIndex)
+                return;
+
+            var length = matchIndex - currentIndex;
+            var result = attributesString.Substring(currentIndex, length).Trim();
+
+            if (result.Length == 0)
+                return;
+
+            //Todo: put massage into resource
+            var message = new StringBuilder();
+            message.AppendLine( "Error while parsing attribute:" );
+            message.AppendLine( attributesString );
+            message.Append( new string( ' ', currentIndex ) );
+            message.Append( new string( '^', length ) );
+            throw new SyntaxException( message.ToString() );
         }
     }
 }
--- src/Tests/AttributeParserTests.cs
+++ src/Tests/AttributeParserTests.cs
@@ -9,37 +9,54 @@ namespace NHaml.Tests
     {
         private static void AssertAttribute(AttributeParser parser, string name, string value, NHamlAttributeType type)
         {
+            AssertAttribute(parser, null, name, value, type);
+        }
+
+        private static void AssertAttribute(AttributeParser parser, string schema, string name, string value,
+                                            NHamlAttributeType type)
+        {
             var attribute = parser.Attributes.FirstOrDefault(a => a.Name == name);
 
             Assert.IsNotNull(attribute, string.Format("Attribute '{0}' not found.", name));
 
-            Assert.AreEqual(value, attribute.Value);
-            Assert.AreEqual(type, attribute.Type);
+            Assert.AreEqual(schema, attribute.Schema, "Schema");
+            Assert.AreEqual(value, attribute.Value, "Value");
+            Assert.AreEqual(type, attribute.Type, "Type");
         }
 
         [Test]
         public void DoubleAndSingleQuotesEncoded()
         {
-            var parser = new AttributeParser(@"b='a\'b\'' d=""\""d\""e""");
+            var parser = new AttributeParser(@"b='a\'b\'' dd=""\""d\""e""");
 
             parser.Parse();
 
             Assert.AreEqual(2, parser.Attributes.Count);
             AssertAttribute(parser, "b", "a\\'b\\'", NHamlAttributeType.String);
-            AssertAttribute(parser, "d", "\\\"d\\\"e", NHamlAttributeType.String);
+            AssertAttribute(parser, "dd", "\\\"d\\\"e", NHamlAttributeType.String);
         }
 
         [Test]
         public void DoubleQuotes()
         {
-            var parser = new AttributeParser("a=\"b\" c=\"d\" e=\"f\" ");
+            var parser = new AttributeParser("a=\"b\" cc=\"d\" eee=\"f\" ");
 
             parser.Parse();
 
             Assert.AreEqual(3, parser.Attributes.Count);
             AssertAttribute(parser, "a", "b", NHamlAttributeType.String);
-            AssertAttribute(parser, "c", "d", NHamlAttributeType.String);
-            AssertAttribute(parser, "e", "f", NHamlAttributeType.String);
+            AssertAttribute(parser, "cc", "d", NHamlAttributeType.String);
+            AssertAttribute(parser, "eee", "f", NHamlAttributeType.String);
+        }
+
+        [Test]
+        public void Empty()
+        {
+            var parser = new AttributeParser("");
+
+            parser.Parse();
+
+            Assert.AreEqual(0, parser.Attributes.Count);
         }
 
         [Test]
@@ -57,68 +74,83 @@ namespace NHaml.Tests
         [Test]
         public void Expressions()
         {
-            var parser = new AttributeParser("a=#{1+1} b=#{\"t\"} c=#{f.ToString()}");
+            var parser = new AttributeParser("aaa=#{1+1} bb=#{\"t\"} c=#{f.ToString()}");
 
             parser.Parse();
 
             Assert.AreEqual(3, parser.Attributes.Count);
-            AssertAttribute(parser, "a", "1+1", NHamlAttributeType.Expression);
-            AssertAttribute(parser, "b", "\"t\"", NHamlAttributeType.Expression);
+            AssertAttribute(parser, "aaa", "1+1", NHamlAttributeType.Expression);
+            AssertAttribute(parser, "bb", "\"t\"", NHamlAttributeType.Expression);
             AssertAttribute(parser, "c", "f.ToString()", NHamlAttributeType.Expression);
         }
 
         [Test]
         public void OnlyReference()
         {
-            var parser = new AttributeParser("a c e");
+            var parser = new AttributeParser("a cc e");
 
             parser.Parse();
 
             Assert.AreEqual(3, parser.Attributes.Count);
             AssertAttribute(parser, "a", "a", NHamlAttributeType.Reference);
-            AssertAttribute(parser, "c", "c", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "cc", "cc", NHamlAttributeType.Reference);
             AssertAttribute(parser, "e", "e", NHamlAttributeType.Reference);
         }
 
         [Test]
         public void ReferenceAsValue()
         {
-            var parser = new AttributeParser("a=b c=d e=f");
+            var parser = new AttributeParser("aa=b c=d eee=f ff=ff.aa ggg=ggg.bb.aa");
+
+            parser.Parse();
+
+            Assert.AreEqual(5, parser.Attributes.Count);
+            AssertAttribute(parser, "aa", "b", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "c", "d", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "eee", "f", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "ff", "ff.aa", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "ggg", "ggg.bb.aa", NHamlAttributeType.Reference);
+        }
+
+        [Test]
+        public void Schema()
+        {
+            var parser = new AttributeParser(@" a:b b:ccc='eee' eee:c=e ");
 
             parser.Parse();
 
             Assert.AreEqual(3, parser.Attributes.Count);
-            AssertAttribute(parser, "a", "b", NHamlAttributeType.Reference);
-            AssertAttribute(parser, "c", "d", NHamlAttributeType.Reference);
-            AssertAttribute(parser, "e", "f", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "a", "b", "b", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "b", "ccc", "eee", NHamlAttributeType.String);
+            AssertAttribute(parser, "eee", "c", "e", NHamlAttributeType.Reference);
         }
 
 
         [Test]
         public void SingleQuotes()
         {
-            var parser = new AttributeParser("a='b' c='d' e='f'");
+            var parser = new AttributeParser("a='b' c='d' ee='f'");
 
             parser.Parse();
 
             Assert.AreEqual(3, parser.Attributes.Count);
             AssertAttribute(parser, "a", "b", NHamlAttributeType.String);
             AssertAttribute(parser, "c", "d", NHamlAttributeType.String);
-            AssertAttribute(parser, "e", "f", NHamlAttributeType.String);
+            AssertAttribute(parser, "ee", "f", NHamlAttributeType.String);
         }
 
         [Test]
         public void SpacesBettwenKeyAndValue()
         {
-            var parser = new AttributeParser("a =a b= b c = 'c'  d  =  #{d} ");
+            var parser = new AttributeParser("a =a bbb= b c = 'c'  dd  =  #{d} ");
 
             parser.Parse();
 
             Assert.AreEqual(4, parser.Attributes.Count);
             AssertAttribute(parser, "a", "a", NHamlAttributeType.Reference);
-            AssertAttribute(parser, "b", "b", NHamlAttributeType.Reference);
+            AssertAttribute(parser, "bbb", "b", NHamlAttributeType.Reference);
             AssertAttribute(parser, "c", "c", NHamlAttributeType.String);
-            AssertAttribute(parser, "d", "d", NHamlAttributeType.Expression);
+            AssertAttribute(parser, "dd", "d", NHamlAttributeType.Expression);
         }
 
         [Test]
@@ -129,52 +161,52 @@ namespace NHaml.Tests
         }
 
         [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
-        public void ThrowExceptionOnForgottenSingleQuoteClose()
-        {
-            new AttributeParser( @" a='text " ).Parse();
-        }
-
-        [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
-        public void ThrowExceptionOnForgottenSingleQuoteOpen()
-        {
-            new AttributeParser( @" a=text' " ).Parse();
-        }
-
-        [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
+        [ExpectedException(typeof (SyntaxException))]
         public void ThrowExceptionOnForgottenDoubleSingleQuoteClose()
         {
-            new AttributeParser( @" a=""text " ).Parse();
+            new AttributeParser(@" a=""text ").Parse();
         }
 
         [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
+        [ExpectedException(typeof (SyntaxException))]
         public void ThrowExceptionOnForgottenDoubleSingleQuoteOpen()
         {
-            new AttributeParser( @" a=text"" " ).Parse();
+            new AttributeParser(@" a=text"" ").Parse();
         }
 
         [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
+        [ExpectedException(typeof (SyntaxException))]
         public void ThrowExceptionOnForgottenExpressionClose()
         {
-            new AttributeParser( @" a=#{text " ).Parse();
+            new AttributeParser(@" a=#{text ").Parse();
         }
 
         [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
+        [ExpectedException(typeof (SyntaxException))]
         public void ThrowExceptionOnForgottenExpressionOpen()
         {
-            new AttributeParser( @" a=text} " ).Parse();
+            new AttributeParser(@" a=text} ").Parse();
         }
 
         [Test]
-        [ExpectedException( typeof( SyntaxException ) )]
+        [ExpectedException(typeof (SyntaxException))]
+        public void ThrowExceptionOnForgottenSingleQuoteClose()
+        {
+            new AttributeParser(@" a='text ").Parse();
+        }
+
+        [Test]
+        [ExpectedException(typeof (SyntaxException))]
+        public void ThrowExceptionOnForgottenSingleQuoteOpen()
+        {
+            new AttributeParser(@" a=text' ").Parse();
+        }
+
+        [Test]
+        [ExpectedException(typeof (SyntaxException))]
         public void ThrowExceptionOnOnlyShema()
         {
-            new AttributeParser( @" a: " ).Parse();
+            new AttributeParser(@" a: ").Parse();
         }
     }
 }
