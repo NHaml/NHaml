56c9b8b7861a7e7e24c6a5c3c385e85f94f840f7
simon.cropp <simon.cropp@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-06-28 05:04:50 +0000

    Add test for issue 51

---
+++ src/Tests/Issues/GC51/Fixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace NHaml.Tests.Issues.GC51
+{
+    [TestFixture]
+    public class Fixture : TestFixtureMvcBase
+    {
+        public Fixture()
+        {
+            ExpectedFolder = TemplatesFolder = @"Issues\GC51\";
+        }
+       
+        [Test]
+        public void UnexpectedFileNotFoundWhenPartialUsedAfterMaster()
+        {
+            AssertView( "Content", "Application", "Output" );
+        }
+    }
+}
