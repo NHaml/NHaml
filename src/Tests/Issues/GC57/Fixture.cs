ce60c79c1f6da48a0337d3e5c5a6d347050accd6
simon.cropp <simon.cropp@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-07-20 12:50:56 +0000

    add failing test for GC57

---
+++ src/Tests/Issues/GC57/Fixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace NHaml.Tests.Issues.GC57
+{
+    [TestFixture]
+    public class Fixture : TestFixtureMvcBase
+    {
+        public Fixture()
+        {
+            ExpectedFolder = TemplatesFolder = @"Issues\GC57\";
+        }
+       
+        [Test]
+        public void MissingNewLine()
+        {
+            AssertView( "Content", "Application", "Output" );
+        }
+    }
+}
