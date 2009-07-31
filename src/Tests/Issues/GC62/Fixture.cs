2cf51a58d61736ab08464eb8018e85e6cab1df15
simon.cropp <simon.cropp@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-07-31 13:36:13 +0000

    Add unit test for GC 51

---
+++ src/Tests/Issues/GC62/Fixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace NHaml.Tests.Issues.GC62
+{
+    [TestFixture]
+    public class Fixture : TestFixtureMvcBase
+    {
+        public Fixture()
+        {
+            ExpectedFolder = TemplatesFolder = @"Issues\GC62\";
+        }
+       
+        [Test]
+        public void MissingIndent()
+        {
+            AssertView( "Content", "Application", "Output" );
+        }
+    }
+}
