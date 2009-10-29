39972ca34ef21fb3b06692c6d8e85e93c2f2e20c
simon.cropp <simon.cropp@7b4b71e7-aa41-0410-a4cf-01f14ca24660>
2009-10-29 12:57:59 +0000

    add more reference to 2010

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -14,9 +14,10 @@ using System.Security.Permissions;
 [assembly: AssemblyFileVersion("1.5.0.2")]
 [assembly: NeutralResourcesLanguage("")]
 [assembly: AllowPartiallyTrustedCallers]
+#if !NET4
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
-
+#endif
 #if !NOT_CLSCOMPLIANT
 [assembly: CLSCompliant(true)]
 #endif
